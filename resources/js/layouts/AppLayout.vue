4d27273597b281b30d314c1589765bc365fe1c1b
eqtada bilhadi <muhamadeqtada@gmail.com>
2025-08-17 21:25:55 +0700

    Handling toast description

--- resources/js/layouts/AppLayout.vue
+++ resources/js/layouts/AppLayout.vue
@@ -21,8 +21,25 @@ watch(
   () => page.props.flash,
   flash => {
     setTimeout(() => {
-      if (flash.success) toast.success(flash.success)
-      if (flash.error) toast.error(flash.error)
+      if (flash.success) {
+        const success = flash.success
+        toast.success(
+          typeof success === "string" ? success : success.title,
+          {
+            description: typeof success === "string" ? undefined : success.description,
+          }
+        )
+      }
+
+      if (flash.error) {
+        const error = flash.error
+        toast.error(
+          typeof error === "string" ? error : error.title,
+          {
+            description: typeof error === "string" ? undefined : error.description,
+          }
+        )
+      }
     }, 5)
   },
   { immediate: true }
